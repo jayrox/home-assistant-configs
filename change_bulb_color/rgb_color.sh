24cf481ea2d22f0714f3295db25ae63489d10a68
Jay Boehm <jayrox@gmail.com>
2016-10-18 21:51:19 -0400

    fix for floats

--- change_bulb_color/rgb_color.sh
+++ change_bulb_color/rgb_color.sh
@@ -6,9 +6,25 @@ HAPASSWORD=$3
 
 LIGHTID=$4
 
+# Define Red and convert to int
 RED=$5
-GREEN=$6
-BLUE=$7
-BRIGHTNESS=$8
+RED=${RED%%.*}
 
-curl $HOST:$PORT/api/services/light/turn_on -X POST -H "x-ha-access: $HAPASSWORD" -H "Content-Type: application/json" -d "{\"entity_id\": \"$LIGHTID\", \"rgb_color\": [$RED,$GREEN,$BLUE], \"brightness\": $BRIGHTNESS}"
+# Define Green and convert to int
+GREEN=$6
+GREEN=${GREEN%%.*}
+
+# Define Blue and convert to int
+BLUE=$7
+BLUE=${BLUE%%.*}
+
+# Define Brightness and convert to int
+BRIGHTNESS=$8
+BRIGHTNESS=${BRIGHTNESS%%.*}
+
+# Gen API URL
+URL=$HOST:$PORT/api/services/light/turn_on
+
+# Call API
+# -k tells curl to not validate SSL
+curl $URL -k -X POST -H "x-ha-access: $HAPASSWORD" -H "Content-Type: application/json" -d "{\"entity_id\": \"$LIGHTID\", \"rgb_color\": [$RED,$GREEN,$BLUE], \"brightness\": $BRIGHTNESS}"
